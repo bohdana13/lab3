7d4aa0e079f0689fc2b1256f4203901d115daf0e
sunshine13067 <thesunshine.201606@example.com>
2025-05-16 15:40:09 +0300

    fix some tests

---
+++ lab9/src/main/java/edu/redko/lab7/request/BookCreateRequest.java
@@ -0,0 +1,10 @@
+package edu.redko.lab7.request;
+
+/*
+  @author User
+  @project lab7
+  @class BookCreateRequest
+  @version 1.0.0
+  @since 02.05.2025 - 14.21
+*/public record BookCreateRequest(String name, String code, String description) {
+}
---
+++ lab9/src/main/java/edu/redko/lab7/request/BookUpdateRequest.java
@@ -0,0 +1,10 @@
+package edu.redko.lab7.request;
+
+/*
+  @author User
+  @project lab7
+  @class BookUpdateRequest
+  @version 1.0.0
+  @since 02.05.2025 - 14.21
+*/public record BookUpdateRequest(String id,String name, String code, String description) {
+}
